6413bd71c9a754ae814108cfa51c671c3c2069cd
louisptdm <57457711+louisptdm@users.noreply.github.com>
2020-01-03 08:35:41 +0100

    JavaDoc --> Joker

--- src/fr/utt/rt/lo02/projet/modele/Joker.java
+++ src/fr/utt/rt/lo02/projet/modele/Joker.java
@@ -2,28 +2,37 @@ package fr.utt.rt.lo02.projet.modele;
 
 // TODO: Auto-generated Javadoc
 /**
- * The Class Joker.
+ * Cette classe définie une unique instance pour la carte de Joker elle 
+ * hérite de la classe Carte.
  */
 public class Joker extends Carte {
 	
-	/** The instance. */
+	/** Création d'une instance unique de JOKER avec comme couleur le carreau.
+	 * Les paramètres sont définis comme tels JOKER comme valeur, CARREAU comme Couleur et Meilleur Jest comme trophée. 
+	 * La couleur de Carreau à été choisie de manière arbitraire.
+	 * Les valeur et le trophées sont correspond directement à la carte JOKER du jeu */
+	
 	private static Joker INSTANCE = new Joker(Valeur.JOKER, Couleur.CARREAU, Trophee.bestJest);
 
 	/**
-	 * Instantiates a new joker.
+	 * Constructeur du joker.
+	 * Cette méthode est un constructeur qui instancie une unique valeur
+	 * qui correspond à une unique carte de JOKER dans le jeu
+	 * Cette carte ne peut être changée que via l'attribut privée INSTANCE de cette même classe 
+	 * 
 	 *
-	 * @param valeur the valeur
-	 * @param couleur the couleur
-	 * @param trophee the trophee
+	 * @param valeur de type de l'énumération Valeur avec comme valeur unique JOKER 
+	 * @param couleur de l'énumération Couleur avec comme valeur choisie arbitrairement CARREAU
+	 * @param trophee de l'énumération Trophée avec comme valeir Meilleur Jest.
 	 */
 	private Joker(Valeur valeur, Couleur couleur, Trophee trophee) {
 		super(valeur, couleur, trophee);
 	}
 
 	/**
-	 * Gets the single instance of Joker.
-	 *
-	 * @return single instance of Joker
+	 * Cette méthode permet d'obtenir l'unique instance de la classe JOKER
+	 * L'instance de Joker à été créee directement dans la classe JOKER via l'attribut INSTANCE.
+	 * @return Cette méthode retourne l'unique instance de la classe JOKER
 	 */
 	public static Joker getInstance() {
 		return INSTANCE;
